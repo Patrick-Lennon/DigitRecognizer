36d915384fdbb8332c15519d161ccb0c7e62b44b
Patrick Lennon <patlennonri@gmail.com>
2018-06-21 18:13:54 -0400

    Prints predicted number and show's the vector
    Only the first element of the array so far.

--- Digimon.py
+++ Digimon.py
@@ -3,5 +3,28 @@ from sklearn.tree import DecisionTreeClassifier
 import numpy as np
 import pandas as pan
 
-data = pan.read_csv('data/train.csv')
-print(data)
+"""
+Using scikit-learn to predict what numbers the hand-written digits are
+
+#author Patrick Lennon
+"""
+
+#panda data converted to a matrix so it's a 2d array
+data = pan.read_csv('data/train.csv').as_matrix()
+clf = DecisionTreeClassifier()
+
+#spliting data into 2: training and prediction
+dataTrain = data[0:21000, 1:]
+tLabel = data[0:21000, 0]
+
+clf.fit(dataTrain, tLabel)
+
+dataTest = data[21000:, 1:]
+aLabel = data[21000, 0]
+
+#taking the virst element (number) converts to a 28x28 matrix vector
+d = dataTest[0]
+d.shape=(28,28)
+plot.imshow(255-d, cmap='Greens')
+print(clf.predict( [dataTest[0]]) )
+plot.show()
